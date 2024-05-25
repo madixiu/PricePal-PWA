73a520a0a5f12df7b7d94cb660f0381d7cd1e26f
madixiu <mahdi.moradi72@gmail.com>
2024-05-25 20:44:35 +0330

    +Appbar +Table +MUI component Lib

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Appbar from './components/Appbar';
+import { createTheme,ThemeProvider } from '@mui/material/styles';
+
+import CurrencyTable from './components/CurrencyTable';
+const themeOptions = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#c3d4e4',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themeOptions}>
+      <div className="App">
+        <Appbar />
+        <CurrencyTable />
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+      </div>
+    </ThemeProvider>
   );
 }
 
