fdf828a292a8a24e297cadf6d299dd97d3200f5f
madixiu <mahdi.moradi72@gmail.com>
2024-06-25 19:37:26 +0330

    *Loading Spinner color

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -5,7 +5,7 @@ import Box from '@mui/material/Box';
 export default function CircularIndeterminate() {
   return (
     <Box sx={{ display: 'flex',justifyContent: 'center',alignItems:'center',flex:1 ,flexDirection:'column',height:'100%'}}>
-      <CircularProgress size={50} sx={{color:'blue'}}/>
+      <CircularProgress size={50} sx={{color:'#C7B40A'}}/>
     </Box>
   );
 }
